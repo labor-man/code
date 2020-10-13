ad0200800342666fd151de827108a11e17e0eb4d
x <997796370@qq.com>
2020-10-13 21:35:18 +0800

    app/web scraping.py

--- app/web scraping.py
+++ app/web scraping.py
@@ -47,3 +47,23 @@ while True:
 
 print('authors:')   
 print(authors)
+
+# Grabbing from muti pages
+print('Grabbing from muti pages...')
+import requests, bs4, os, send2trash
+
+# 应对豆瓣反爬，加上User-Agent Header
+user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:68.0) Gecko/20100101 Firefox/68.0"
+r = requests.get('https://movie.douban.com/j/search_subjects?type=movie&tag=%E6%AC%A7%E7%BE%8E&sort=recommend&page_limit=1000&page_start=0', headers = {'User-Agent':user_agent})
+
+dir = os.path.expanduser('~/Desktop/temp')
+if os.path.exists(dir):
+	send2trash.send2trash(dir)
+
+os.mkdir(dir)
+os.chdir(dir)
+for subject in r.json()['subjects']:
+    image_link = requests.get(subject['cover'])
+    print(subject['title'])
+    with open(subject['title'].replace('/', u'\u2215') + os.path.splitext(subject['cover'])[1], 'wb') as f:
+        f.write(image_link.content)
