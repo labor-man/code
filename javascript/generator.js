a27b41d6d49d7ec0bfbb69b3128a203a872c4157
x <997796370@qq.com>
2020-12-03 19:51:15 +0800

    ~

--- javascript/generator.js
+++ javascript/generator.js
@@ -32,3 +32,48 @@ controller(function* () {
   const c = yield curriedFoo('c');
   console.log(c);
 });
+
+// with error handling
+const foo = (parameters, callback) => {
+    setTimeout(() => {
+        console.log('callback', callback);
+        callback(parameters);
+    }, 100);
+};
+
+
+const controller = (generator) => {
+    const iterator = generator();
+
+    const advancer = (response) => {
+        if (response && response.error) {
+            return iterator.throw(response.error);
+        }
+
+        const state = iterator.next(response);
+
+        if (!state.done) {
+            console.log('state.value:', state.value);
+            console.log('advancer:', advancer);
+            state.value(advancer);
+        }
+    }
+
+    advancer();
+};
+
+controller(function* () {
+    let a,
+        b,
+        c;
+
+    try {
+        a = yield _.curry(foo)('a');
+        b = yield _.curry(foo)({error: 'Something went wrong.'});
+        c = yield _.curry(foo)('c');
+    } catch (e) {
+        console.log(e);
+    }
+
+    console.log(a, b, c);
+});
